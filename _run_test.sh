e82e28aeea201cce78b25e086c059ae22ba0a495
thanhzro <thanhtran2609zro@gmail.com>
2026-05-12 19:17:48 +0700

    feat: _run_test.sh with timing

--- _run_test.sh
+++ _run_test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 TEST=$1
-./t $TEST 2>/dev/null | sed 's/[[:space:]]*$//' > _ref.txt
-./t_bc $TEST 2>/dev/null | sed 's/[[:space:]]*$//' > _bc.txt
-diff _ref.txt _bc.txt >/dev/null && echo PASS || echo FAIL
+{ time bash -c "./t $TEST 2>/dev/null | sed 's/[[:space:]]*\$//' > _ref.txt && ./t_bc $TEST 2>/dev/null | sed 's/[[:space:]]*\$//' > _bc.txt && diff _ref.txt _bc.txt >/dev/null && echo PASS || echo FAIL"; } 2>&1 | grep -E "PASS|FAIL|real"
